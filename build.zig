4570fe5741a11b8e034086f80dd6c9f7357b909f
Jassiel Ovando <jassielovando@protonmail.com>
2026-03-13 14:38:11 -0400

    tests

--- build.zig
+++ build.zig
@@ -26,7 +26,9 @@ pub fn build(b: *std.Build) void {
     const docs_step = b.step("docs", "Generate the documentation");
     docs_step.dependOn(&docs.step);
 
-    const tests = b.addTest(.{
+    const tests_step = b.step("tests", "Run the test suite");
+
+    const integration_tests = b.addTest(.{
         .root_module = b.createModule(.{
             .root_source_file = b.path("tests/suite.zig"),
             .target = target,
@@ -38,7 +40,13 @@ pub fn build(b: *std.Build) void {
         }),
     });
 
-    const test_step = b.step("tests", "Run the test suite");
-    const run_tests = b.addRunArtifact(tests);
-    test_step.dependOn(&run_tests.step);
+    const run_integration_tests = b.addRunArtifact(integration_tests);
+    tests_step.dependOn(&run_integration_tests.step);
+
+    const unit_tests = b.addTest(.{
+        .root_module = lib_mod,
+    });
+
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+    tests_step.dependOn(&run_unit_tests.step);
 }
