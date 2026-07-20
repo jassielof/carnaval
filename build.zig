2d866710354fd4b8195e0df4a6f94f44e42d0cc7
Jassiel Ovando <jassielovando@protonmail.com>
2026-07-20 18:59:59 -0400

    naming

--- build.zig
+++ build.zig
@@ -4,25 +4,25 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib_mod = b.addModule("carnaval", .{
+    const carnaval_mod = b.addModule("carnaval", .{
         .root_source_file = b.path("lib/carnaval/root.zig"),
         .optimize = optimize,
         .target = target,
     });
 
-    const lib = b.addLibrary(.{
+    const carnaval_mod_lib = b.addLibrary(.{
         .name = "carnaval",
-        .root_module = lib_mod,
+        .root_module = carnaval_mod,
     });
 
-    const docs = b.addInstallDirectory(.{
-        .source_dir = lib.getEmittedDocs(),
+    const carnaval_docs = b.addInstallDirectory(.{
+        .source_dir = carnaval_mod_lib.getEmittedDocs(),
         .install_dir = .prefix,
         .install_subdir = "docs",
     });
 
     const docs_step = b.step("docs", "Generate the documentation");
-    docs_step.dependOn(&docs.step);
+    docs_step.dependOn(&carnaval_docs.step);
 
     const tests_step = b.step("test", "Run the test suite");
 
@@ -34,7 +34,7 @@ pub fn build(b: *std.Build) void {
             .optimize = optimize,
             .imports = &.{.{
                 .name = "carnaval",
-                .module = lib_mod,
+                .module = carnaval_mod,
             }},
         }),
     });
@@ -42,13 +42,13 @@ pub fn build(b: *std.Build) void {
     const run_integration_tests = b.addRunArtifact(integration_tests);
     tests_step.dependOn(&run_integration_tests.step);
 
-    const unit_tests = b.addTest(.{
-        .name = "Unit",
-        .root_module = lib_mod,
+    const carnaval_mod_lib_tests = b.addTest(.{
+        .name = "Carnaval",
+        .root_module = carnaval_mod,
     });
 
-    const run_unit_tests = b.addRunArtifact(unit_tests);
-    tests_step.dependOn(&run_unit_tests.step);
+    const run_carnaval_mod_lib_tests = b.addRunArtifact(carnaval_mod_lib_tests);
+    tests_step.dependOn(&run_carnaval_mod_lib_tests.step);
 
     const check_step = b.step("check", "Run code quality checks");
 
